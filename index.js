637a4c232a49f49dc0b898d7da65fbe97926c85b
VigneshKasa <vigneshkasa27@gmail.com>
2023-12-16 20:42:05 +0530

    added sound effects

--- index.js
+++ index.js
@@ -14,17 +14,37 @@
         //2==paper
         //3==scissors
      if((a==1 && x==3)||(a==2 && x==1)||(a==3 && x==2)){
+        if((a==1)){
+            let sound=new Audio("rock.mp3")
+            sound.play()
+        }
+        if((a==3)){
+            let sound=new Audio("scissors.mp3")
+            sound.play()
+        }
+        if((a==2)){
+            let sound=new Audio("paper.mp3")
+            sound.play()
+        }
+        setTimeout(() => {
+            let sound=new Audio("win.mp3")
+            sound.play()
+        },1000);
         document.getElementById("result").innerHTML="You win 🏆"
         }
+        
         else if((a==1 && x==2)||(a==2 && x==3)||(a==3 && x==1)){
+            let sound=new Audio("lost.mp3")
+            sound.play()
             document.getElementById("result").innerHTML="You Lost ❌"
             }
             else{
-                document.getElementById("result").innerHTML="Tie 🙃"  
+                    let sound=new Audio("tie.mp3")
+                    sound.play()
+                    document.getElementById("result").innerHTML="Tie 🙃"  
             }
 
             document.getElementById("msg").innerHTML="Opponent selected "+img
             document.getElementById("reload").innerHTML="Play Again !"  
-
     }
                 
